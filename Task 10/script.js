3ee16badaf1f238a22a6805137f11f781f389a87
osama-25 <osamasami208@gmail.com>
2024-06-14 21:21:25 +0300

    adding task 10

---
+++ Task 10/script.js
@@ -0,0 +1,6 @@
+function getip(){
+    let button = document.getElementById("button");
+    let fetchRes = fetch("https://api.ipify.org?format=json")
+    .then(response => response.json())
+    .then(data => button.innerHTML = data.ip);
+}
